68312422372b8b02fba9f85fe97132d16b1531a3
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2024-11-20 17:01:54 +0700

    Updated style.css, index.html, and script.js files with minor changes to CSS, added new section elements, and modified JavaScript event listeners.

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ if(navToggle){
         navMenu.classList.add('show-menu')
     })
 }
-
-
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
+
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// const scrollHeader = () => {
+//     const header = document.getElementById('header')
+//     this .scrollY > 200 ? header.classList.add('scroll-header') 
+//                         : header.classList.remove('scroll-header')
+// }
