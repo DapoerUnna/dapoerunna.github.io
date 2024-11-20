5d1f307c7017e6ed15f56cd594412468a3a62046
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2024-11-20 22:26:52 +0700

    Added footer section to HTML, updated CSS styles, and modified JavaScript to add blur effect to header on scroll

--- script.js
+++ script.js
@@ -23,8 +23,9 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-// const scrollHeader = () => {
-//     const header = document.getElementById('header')
-//     this .scrollY > 200 ? header.classList.add('scroll-header') 
-//                         : header.classList.remove('scroll-header')
-// }
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
