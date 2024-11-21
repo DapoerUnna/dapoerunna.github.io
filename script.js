70c681df0509e06ba26085233e820b5067ff6d3a
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2024-11-21 09:35:57 +0700

    Updated CSS styles for navigation menu and adjusted JavaScript code for navigation toggle and link actions.

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+        navMenu.classList.add('show-menu');
+    });
 }
-if(navClose){
+
+if (navClose) {
     navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+        navMenu.classList.remove('show-menu');
+    });
 }
 
-
-const navLink = document.querySelectorAll('.nav_link')
+const navLink = document.querySelectorAll('.nav_link');
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+    navMenu.classList.remove('show-menu');
+};
 
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 const blurHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('blur-header') 
-                        : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+    const header = document.getElementById('header');
+    window.scrollY >= 50 ? header.classList.add('blur-header') 
+                         : header.classList.remove('blur-header');
+};
+
+window.addEventListener('scroll', blurHeader);
